b105942f7bc8151a1f0dd1ad7936fa4210abc58d
zaki <abdullah.muzaki@alodokter.com>
2022-05-06 06:43:20 +0700

    Remove unused

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { useHistory } from 'react-router'
-import '../style.css'
+import { useNavigate } from 'react-router'
 import {
   useLocation
 } from "react-router-dom";
@@ -10,10 +9,7 @@ function Card({el}) {
 
   let location = useLocation();
 
-
-  const [deleteMovieById, { data: deletedMovie }] = useMutation(DELETE_MOVIE)
-
-  let history = useHistory()
+  let history = useNavigate()
 
   const handleEdit = (e) => {
     let path = ('/movies/edit/' + el._id)
@@ -27,36 +23,14 @@ function Card({el}) {
 
   const handleDelete = (e) => {
     console.log(e)
-
-    deleteMovieById({
-      variables : {
-        "input": el._id
-      },
-      refetchQueries : [{
-        query: GET_ALL_MOVIES
-      }]
-    })
   }
-
-  if (location.pathname === "/movies") return (
-    <div className="card m-2 clickable-card" style={{width: '18rem'}} >
-      {/* <img src="..." className="card-img-top" alt="..." /> */}
-      <div className="card-body">
-        <h5 className="card-title">{el.title}</h5>
-        <p className="card-text">{el.overview}</p>
-        <p className="card-text">{el.poster_path}</p>
-        <p className="card-text">{el.popularity}</p>
-        <button className="btn btn-primary m-1" onClick={ handleEdit }  >Edit</button>
-        <button className="btn btn-danger m-1" onClick={ handleDelete } >Delete</button>
-      </div>
-    </div>
-  );
 
   return (
     <div className="card m-2 clickable-card" style={{width: '18rem'}} >
       {/* <img src="..." className="card-img-top" alt="..." /> */}
       <div className="card-body">
         <h5 className="card-title">{el.title}</h5>
+        <h1>sds</h1>
         <p className="card-text">{el.overview}</p>
       </div>
     </div>
---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../components/Card'
+
+function Home() {
+
+  if (1) return (
+    <div className="d-flex flex-wrap" >
+      <Card></Card>
+    </div>
+  )
+}
+
+export default Home
