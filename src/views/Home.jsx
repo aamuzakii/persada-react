312e539eb8e2b5db1188304ba0f33a0e0fbef843
zaki <abdullah.muzaki@alodokter.com>
2022-05-06 07:42:25 +0700

    Create UI: Navbar, BG, Card

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+// import Card from '../components/Card'
+import Navbar from '../components/Navbar'
 import Card from '../components/Card'
+import { Container, Button, Box } from '@mui/material';
 
 function Home() {
 
   if (1) return (
     <div className="d-flex flex-wrap" >
-      <Card></Card>
+      <Navbar/>
+      <Box
+        component="img"
+        sx={{
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+        }}
+        alt="The house from the offer."
+        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+      />
+      <Container>
+        <Card>
+        </Card>
+        
+      </Container>
     </div>
   )
 }
