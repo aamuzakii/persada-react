28c11a6d05c8e5a3642da0587cbc33faa62bbc12
zaki <abdullah.muzaki@alodokter.com>
2022-05-08 21:24:12 +0700

    Router safe point

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
+import { Switch, Route } from 'react-router-dom'
 import Home from './views/Home'
 
 function App() {
   return (
     <>
-      {/* <Navbar/> */}
-      {/* <Routes> */}
-        {/* <Route exact path="/" children={ <Home/> } /> */}
-      {/* </Routes> */}
-      <Home></Home>
+      <Switch>
+        <Route exact path="/asas" >
+          <Home></Home>
+        </Route>
+
+        <Route exact path="/recommendation" >
+        </Route>
+
+        <Route exact path="/favourite" >
+        </Route>
+
+        <Route exact path="/companies/:id" >
+        </Route>
+      </Switch>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from './store'
-
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 
@@ -13,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
