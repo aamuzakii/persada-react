b2d1a25da232614bc503c852965f89f8810afd2c
zaki <abdullah.muzaki@alodokter.com>
2022-05-22 21:15:29 +0700

    Finish Style Card

--- src/views/Orders.jsx
+++ src/views/Orders.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChipsRow from '../components/orders/ChipsRow'
 import EmptyOrder from '../components/orders/EmptyOrder'
 import NavbarOrder from '../components/orders/Appbar'
+import Card from '../components/orders/Card'
 
 function Orders() {
+  function fetchOrderByStatus() {
+    console.log(2)
+    let url = 'http://localhost:3000/orders/index_by_status?status=testing bro'
+  
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        // dispatch(setProductByCategory(result))
+      })
+      .catch(error => console.error('error', error));
+  }
+
+  useEffect(() => {
+    console.log(11)
+    fetchOrderByStatus()
+  }, [])
+  
+  
+
   return (
     <>
       <NavbarOrder></NavbarOrder>
@@ -12,7 +38,8 @@ function Orders() {
         <div style={{ fontWeight: 'bold', color: 'rgb(102, 102, 102)' }} >Pesanan Saya</div>
       </div>
         <ChipsRow></ChipsRow>
-        <EmptyOrder></EmptyOrder>
+        {/* <EmptyOrder></EmptyOrder> */}
+        <Card></Card>
     </>
     
   )
