08fee8501490787654ab4a57c708f6cb2def2ed1
zaki <abdullah.muzaki@alodokter.com>
2022-05-22 22:32:02 +0700

    trigger AWS to re build

--- src/components/SharedStyle.js
+++ src/components/SharedStyle.js
@@ -4,3 +4,4 @@ export const elevatedContainerLeft = { display: 'flex', flexDirection: 'column',
 export const elevatedContainerCenter = { display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 12, boxShadow: 'rgb(0 0 0 / 14%) 0px 1px 4px' }
 export const miniGreyFont = { fontWeight: '400', fontSize: '12px', color: 'rgb(102, 102, 102)', letterSpacing: '0.5px' }
 export const miniBoldFont = { fontWeight: 'bold', fontSize: '0.8rem', color: 'rgb(102, 102, 102)' }
+export const foo = 'bar'
